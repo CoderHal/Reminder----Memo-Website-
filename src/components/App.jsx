9bc6aac53681bf21d6537b24e017840655f5859c
YWcoding <youhao1997@outlook.com>
2022-07-31 15:56:08 -0400

    Implement Add Note function and Delete Notes funciton

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import notes from "../notes";
+import CreateArea from "./CreateArea";
 
 function App(){
+  const [notes, setNotes] = useState([]);
+
+  function addItem(newNotes){
+    setNotes(prevNotes=>{
+      return [...prevNotes,newNotes]
+    })
+  }
+
+  function deleteNote(id) {
+    setNotes(prevNotes=>{
+      return (prevNotes.filter((items, index)=>{
+        return id !== index;
+      }))
+    })
+  }
+  
   return (
     <div>
       <Header />
-      {notes.map(note=>{
-        return <Note 
-          key = {note.id}
-          title = {note.title}
-          content = {note.content}
-        />
+      <CreateArea onAdd={addItem}/>
+      {notes.map((noteItem,index)=>{
+        return (
+          <Note key= {index} 
+          id = {index}
+          title={noteItem.title}
+          content={noteItem.content}
+          onDelete={deleteNote}
+          />
+        )
       })}
       <Footer />
     </div>
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 
 function Note(props) {
+
+
   return (
     <div className="note">
       <h1>{props.title}</h1>
       <p> {props.content}</p>
+      <button onClick={()=>{
+        return (
+          props.onDelete(props.id)
+        );
+      }}>DELETE</button>
     </div>
   );
 }
