a1d2e3fc0f46b2153330aa8772a8e1249460b05f
CoderHal <youhao1997@outlook.com>
2022-08-05 11:34:24 -0400

    Design the frontend interface

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,35 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 
-const App =() => {
+import reminders from "./images/reminders.png";
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+import useStyles from "./styles";
+
+const App = () => {
+
+    const classes = useStyles();
+
     return (
-        <div>
-            <h1>App</h1>
-        </div>
-    )
+        <Container madxidth="lg">
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center">Reminders</Typography>
+                    <img className={classes.image} src={reminders} alt="reminders" height="60" />
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch" spacing={4}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
+    );
 }
 
 export default App;
