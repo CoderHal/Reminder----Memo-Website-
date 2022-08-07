fed5765e3dabfded161a7bfdc66d8702826cce17
CoderHal <youhao1997@outlook.com>
2022-08-07 11:20:47 -0400

    implement like function

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -6,7 +6,7 @@ import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from "moment"
 import useStyles from "./styles";
 import { useDispatch } from "react-redux";
-import {deletePost} from "../../../actions/posts"
+import {deletePost, likePost } from "../../../actions/posts"
 
 const Post = ({ post,setCurrentId }) => {
     const classes = useStyles();
@@ -31,15 +31,14 @@ const Post = ({ post,setCurrentId }) => {
                 <Typography variant="h5" gutterBottom>{post.message}</Typography>
             </CardContent>
             <CardActions className={classes.cardActions}>
-                <Button size="small" color="primary" onClick={()=>{}}>
+                <Button size="small" color="primary" onClick={()=>dispatch(likePost(post._id))}>
                     <ThumbUpAltIcon fontSize="small"/>
                     Like
-                    {post.lickCount}
+                    {post.likeCount}
                 </Button>
                 <Button size="small" color="primary" onClick={()=>dispatch(deletePost(post._id))}>
                     <DeleteIcon fontSize="small"/>
                     Delete
-                    {post.lickCount}
                 </Button>
 
             </CardActions>
